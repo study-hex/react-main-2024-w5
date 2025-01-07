4409872109e84c83b6200bea86ba8460cfd41c80
Aya <90342033+Aya-X@users.noreply.github.com>
2025-01-07 12:00:53 +0800

    chore: Update Vite config base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/react-main-2024-w2",
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
