9a717dc539718b1be07ebfd7724972b3d6a0d69b
Aya <90342033+Aya-X@users.noreply.github.com>
2025-01-08 14:24:44 +0800

    chore: Update Vite configuration to load environment variables and set up proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,35 @@
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, type ConfigEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path"
 
-export default defineConfig({
-  base: "/react-main-2024-w2/",
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
+export default ({ mode }: ConfigEnv) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    server: {
+      open: true,
+      proxy: {
+        '/v2': {
+          target: process.env.VITE_APP_API_URL,
+          changeOrigin: true,
+        },
+      },
     },
-  },
-  plugins: [reactRouter(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./app"),
+    base: "/react-main-2024-w2/",
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
+      },
     },
-  },
-});
+    plugins: [reactRouter(), tsconfigPaths()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./app"),
+      },
+    },
+  });
+}
+
