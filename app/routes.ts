328d9d3538b0632e8c6089af4d4ee55b912b5492
Aya <90342033+Aya-X@users.noreply.github.com>
2025-01-13 21:28:10 +0800

    chore: Update project name and routes to version 2024-w4

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,5 @@
 import { type RouteConfig, index, prefix } from "@react-router/dev/routes";
 
-export default prefix("/react-main-2024-w3", [
+export default prefix("/react-main-2024-w4", [
     index("routes/home.tsx")
 ]) satisfies RouteConfig;
